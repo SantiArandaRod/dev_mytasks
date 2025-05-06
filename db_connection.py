144ae78b2be4b4344f4762faa8bbe8c9a40d3910
dsar <dsaranda89@ucatolica.edu.co>
2025-05-06 11:23:02 -0500

    fix addons

--- db_connection.py
+++ db_connection.py
@@ -5,24 +5,25 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import  AsyncSession
 from sqlalchemy.orm import sessionmaker
-
+#from .env import *
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./database.db")
-engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+DATABASE_URL = (
+    f"postgresql+asyncpg://{os.environ['POSTGRESQL_ADDON_USER']}:"
+    f"{os.environ['POSTGRESQL_ADDON_PASSWORD']}@"
+    f"{os.environ['POSTGRESQL_ADDON_HOST']}:"
+    f"{os.environ['POSTGRESQL_ADDON_PORT']}/"
+    f"{os.environ['POSTGRESQL_ADDON_DB']}"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-POSTGRESQL_ADDON_HOST="blrds6moy36ytber4epr-postgresql.services.clever-cloud.com"
-POSTGRESQL_ADDON_DB="blrds6moy36ytber4epr"
-POSTGRESQL_ADDON_USER="ub73yioakc05zqt9meah"
-POSTGRESQL_ADDON_PORT=5432
-POSTGRESQL_ADDON_PASSWORD="XhCtvMSYLlU4w5z5KpXIzYeSvIUxuc"
-POSTGRESQL_ADDON_URI="postgresql://ub73yioakc05zqt9meah:XhCtvMSYLlU4w5z5KpXIzYeSvIUxuc@blrds6moy36ytber4epr-postgresql.services.clever-cloud.com:5432/blrds6moy36ytber4epr"
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
-
-async def get_session():
-    async with async_session() as session:
-        yield session
