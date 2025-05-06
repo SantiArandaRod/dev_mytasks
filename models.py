06e53837efc5d83e7fde1fe99f6a69f1307b7e37
dsar <dsaranda89@ucatolica.edu.co>
2025-05-05 19:52:49 -0500

    Add modelSQL for each user & task

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from pydantic import ConfigDict
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 from enum import Enum
@@ -13,17 +16,26 @@ class UserStatus(str, Enum):
     i = "inactivo"
     d = "eliminado"
 
-class UserBase(SQLModel, table=True):
+class UserBase(SQLModel):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True, min_length=1, max_length=100)
     email: str = Field(index=True, unique=True, max_length=100)
     status: UserStatus = Field(default=UserStatus.a)
     premium: bool = Field(default=False)
 
-class TaskBase(SQLModel, table=True):
+class TaskBase(SQLModel):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True, min_length=1, max_length=100)
     description: str = Field(max_length=100)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
     status: TaskStatus = Field(default=TaskStatus.p)
+
+class TaskSQL(SQLModel, table=True):
+    __tablename__ = "tasks"
+    id: Optional[int]= Field(default=None, primary_key=True)
+    model_config = ConfigDict(from_attributes=True)
+class UserSQL(SQLModel, table=True):
+    __tablename__ = "users"
+    id: Optional[int]= Field(default=None, primary_key=True)
+    model_config = ConfigDict(from_attributes=True)
