fe2704571f8947283e66166164b2aa79905cfce7
dsar <dsaranda89@ucatolica.edu.co>
2025-05-06 07:31:34 -0500

    add addons onlineDB

--- db_connection.py
+++ db_connection.py
@@ -12,6 +12,13 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./database.db")
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+POSTGRESQL_ADDON_HOST="blrds6moy36ytber4epr-postgresql.services.clever-cloud.com"
+POSTGRESQL_ADDON_DB="blrds6moy36ytber4epr"
+POSTGRESQL_ADDON_USER="ub73yioakc05zqt9meah"
+POSTGRESQL_ADDON_PORT=5432
+POSTGRESQL_ADDON_PASSWORD="XhCtvMSYLlU4w5z5KpXIzYeSvIUxuc"
+POSTGRESQL_ADDON_URI="postgresql://ub73yioakc05zqt9meah:XhCtvMSYLlU4w5z5KpXIzYeSvIUxuc@blrds6moy36ytber4epr-postgresql.services.clever-cloud.com:5432/blrds6moy36ytber4epr"
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
